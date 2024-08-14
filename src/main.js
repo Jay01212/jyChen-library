2e6878cf796e69a656d11c49d3db4d7f50a2d5d6
Jay01212 <jche0295@student.monash.edu>
2024-08-15 02:31:27 +1000

    Week6-Activity456-done

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createApp } from "vue"
 import App from "./App.vue"
 
 //import JSON from '../src/components/JSON.vue'
-import './style.css'
+//import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
